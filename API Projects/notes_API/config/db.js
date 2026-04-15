8bc6b6b5387b9fd166b2700fc8763e36d258d1bf
Pranav kumar reddy k <kondapurampranav@gmail.com>
2026-04-15 21:59:11 +0530

    notes API

--- API Projects/notes_API/config/db.js
+++ API Projects/notes_API/config/db.js
@@ -1,12 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Pranav@34", 
-    database: "tasksdb",
-    waitForConnections: true,
-    connectionLimit: 10
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = pool;
