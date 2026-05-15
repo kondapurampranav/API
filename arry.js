ed2019bab38e427aa7e308806c7013bdb7a74846
Pranav kumar reddy k <kondapurampranav@gmail.com>
2026-05-15 21:45:53 +0530

    reorder

---
+++ arry.js
@@ -0,0 +1,12 @@
+
+const time1 = Date.now()
+arr = [1, 2, "hello", "by"]
+
+console.log(arr)
+
+
+const time2 = Date.now();
+console.log(" ")
+console.log(time1);
+console.log(time2);
+console.log(time2 - time1);
